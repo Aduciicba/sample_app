22363b1c423966440bb45f69acb422ee65c4f91b
Aduciicba <sfera23@yandex.ru>
2016-01-08 03:09:28 +0300

    add some erb

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "home_page" do
+    it "must have home page" do
+      get static_pages_home_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "must have text Sample_App" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample_App')
+    end
+
+    it "must have Home in title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')
+    end
+  end
+
+  describe "help page" do
+    it "must have help page" do
+      get static_pages_help_path
+      expect(response).to have_http_status(200)
+    end
+
+    it "must have Help in title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')
+    end
+  end
+
+  describe "about page" do
+    it "must have about page" do
+      get static_pages_about_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end
