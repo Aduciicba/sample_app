d325b424cf8ac8006dafede6de6800e8178cf289
Aduciicba <sfera23@yandex.ru>
2016-01-11 03:34:48 +0300

    finished fill layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,44 +2,47 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe "home_page" do
+
     it "must have home page" do
-      get static_pages_home_path
+      get root_path
       expect(response).to have_http_status(200)
     end
 
-    it "must have text Sample_App" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample_App')
+    before {visit root_path}
+
+    it "must have text SampleApp" do
+      #visit static_pages_home_path
+      expect(page).to have_content('SampleApp')
     end
 
     it "must have Home in title" do
-      visit '/static_pages/home'
+      #visit static_pages_home_path
       expect(page).to have_title('Home')
     end
   end
 
   describe "help page" do
     it "must have help page" do
-      get static_pages_help_path
+      get help_path
       expect(response).to have_http_status(200)
     end
 
     it "must have Help in title" do
-      visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      visit help_path
+      expect(page).to have_title('Help')
     end
   end
 
   describe "about page" do
     it "must have about page" do
-      get static_pages_about_path
+      get about_path
       expect(response).to have_http_status(200)
     end
   end
 
   describe "contact page" do
     it "must have contact page" do
-      get static_pages_contact_path
+      get contact_path
       expect(response).to have_http_status(200)
     end
   end
